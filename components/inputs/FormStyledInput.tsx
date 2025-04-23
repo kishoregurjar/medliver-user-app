411e8f38be16258d59ad911b85a79a1187170d56
Yash Tiwari <124504660+yash-kumar-tiwari@users.noreply.github.com>
2025-04-23 12:10:38 +0530

    Update FormStyledInput.tsx

--- components/inputs/FormStyledInput.tsx
+++ components/inputs/FormStyledInput.tsx
@@ -1,11 +1,20 @@
-import { TextInput } from "react-native";
+import { TextInput, TextInputProps } from "react-native";
 
-const FormStyledInput = (props: any) => {
+interface FormStyledInputProps extends TextInputProps {
+  className?: string;
+  placeholderTextColor?: string;
+}
+
+const FormStyledInput = ({
+  className = "border border-app-color-warmgreylight font-lexend rounded-md px-4 py-3 text-base", // remove bold here
+  placeholderTextColor = "#6E6A7C", // softer gray
+  ...rest
+}: FormStyledInputProps) => {
   return (
     <TextInput
-      className="border border-app-color-warmgreylight rounded-md px-4 py-3 font-lexend-black"
-      placeholderTextColor="#999"
-      {...props}
+      className={className}
+      placeholderTextColor={placeholderTextColor}
+      {...rest}
     />
   );
 };
